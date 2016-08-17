1790969d8e852026743e023ae1d30fe8c20f4fa5
ideaoforder <mark@sitesteaders.com>
2016-08-17 15:04:51 -0400

    urlencode SKU on items#sku

--- whiplash_api.php
+++ whiplash_api.php
@@ -20,7 +20,7 @@ class WhiplashApi
         } else {
             $this->base_url = 'https://www.whiplashmerch.com/api/';
         }
-        
+
         $ch = curl_init();
     	// Set headers
         $headers = array('Content-type: application/json', 'Accept: application/json', "X-API-KEY: $api_key");
@@ -39,7 +39,7 @@ class WhiplashApi
         // the $path sometimes has the "?" character included.  this is part of the Whiplash API
         // if the path includes "?" character and there are $params then this function will need to be updated
         // for more robust construction of the $json_url
-        
+
         // for now this prevents the ? from being added twice
         if ($query_params) {
             $json_url .= '?' . $query_params;
@@ -51,9 +51,9 @@ class WhiplashApi
         $out = json_decode($result); // Decode the result
         return $out;
     }
-    
+
     public function post($path, $params=array()) {
-        $json_url = $this->base_url . $path; 
+        $json_url = $this->base_url . $path;
         $ch = $this->connection;
         curl_setopt($ch, CURLOPT_URL, $json_url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
@@ -62,9 +62,9 @@ class WhiplashApi
         $out = json_decode($result); // Decode the result
         return $out;
     }
-    
+
     public function put($path, $params=array()) {
-        $json_url = $this->base_url . $path; 
+        $json_url = $this->base_url . $path;
         $ch = $this->connection;
         curl_setopt($ch, CURLOPT_URL, $json_url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
@@ -73,9 +73,9 @@ class WhiplashApi
         $out = json_decode($result); // Decode the result
         return $out;
     }
-    
+
     public function delete($path, $params=array()) {
-        $json_url = $this->base_url . $path; 
+        $json_url = $this->base_url . $path;
         $ch = $this->connection;
         curl_setopt($ch, CURLOPT_URL, $json_url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
@@ -83,7 +83,7 @@ class WhiplashApi
         $out = json_decode($result); // Decode the result
         return $out;
     }
-            
+
     /** Item functions **/
     public function get_items($params=array()) {
         return $this->get('items', $params);
@@ -92,23 +92,23 @@ class WhiplashApi
     public function get_items_count($params=array()) {
         return $this->get('items/count', $params);
     }
-    
+
     public function get_item($id) {
         return $this->get('items/'.$id);
     }
-    
+
     public function get_item_transactions($id) {
         return $this->get('items/'.$id.'/transactions');
     }
-    
+
     public function get_items_by_sku($sku, $params=array()) {
-        return $this->get('items/sku/'.$sku, $params);
+        return $this->get('items/sku/'.urlencode($sku), $params);
     }
-    
+
     public function get_item_by_originator($id) {
         return $this->get('items/originator/'.$id);
     }
-    
+
     // This requires a valid ID
     public function create_item($params=array()) {
         $p = array();
@@ -120,7 +120,7 @@ class WhiplashApi
         }
         return $this->post('items', $p);
     }
-    
+
     // This requires a valid ID
     public function update_item($id, $params=array()) {
         $p = array();
@@ -131,13 +131,13 @@ class WhiplashApi
         }
         return $this->put('items/'.$id, $p);
     }
-    
+
     // This requires a valid ID
     public function delete_item($id) {
         return $this->delete('items/'.$id);
     }
-    
-    
+
+
     /** Order functions **/
     public function get_orders($params=array()) {
         return $this->get('orders', $params);
@@ -146,23 +146,23 @@ class WhiplashApi
     public function get_orders_count($params=array()) {
         return $this->get('orders/count', $params);
     }
-    
+
     public function get_order($id) {
         return $this->get('orders/'.$id);
     }
-    
+
     public function get_order_items($id) {
         return $this->get('order_items?order_id='.$id);
     }
-    
+
     public function get_order_by_originator($id) {
         return $this->get('orders/originator/'.$id);
     }
-    
+
     public function get_order_by_status($status) {
         return $this->get('orders/status/'.$status);
     }
-    
+
     // This requires a valid ID
     public function create_order($params=array()) {
         $p = array();
@@ -180,7 +180,7 @@ class WhiplashApi
         }
         return $this->post('orders', $p);
     }
-    
+
     // This requires a valid ID
     public function update_order($id, $params=array()) {
         $p = array();
@@ -191,7 +191,7 @@ class WhiplashApi
         }
         return $this->put('orders/'.$id, $p);
     }
-    
+
     // possibly rename this to "cancel_order" since "delete" is not supported by the api
     public function delete_order($id) {
         return $this->put('orders/'.$id.'/cancel'); // orders/{order-id}/cancel
@@ -201,26 +201,26 @@ class WhiplashApi
     public function undelete_order($id) {
         return $this->put('orders/'.$id.'/uncancel'); // orders/{order-id}/uncancel
     }
-    
+
     // This requires a valid ID
     public function pause_order($id) {
         return $this->put('orders/'.$id.'/pause'); // orders/{order-id}/pause
     }
-    
+
     // This requires a valid ID
     public function unpause_order($id) {
         return $this->put('orders/'.$id.'/release'); // orders/{order-id}/release
     }
 
-    /** OrderItem functions **/		
+    /** OrderItem functions **/
     public function get_order_item($id) {
         return $this->get('order_items/'.$id);
     }
-    
+
     public function get_order_item_by_originator($id) {
         return $this->get('order_items/originator/'.$id);
     }
-    
+
     // This requires a valid ID
     public function create_order_item($params=array()) {
         $p = array();
@@ -231,7 +231,7 @@ class WhiplashApi
         }
         return $this->post('order_items', $p);
     }
-    
+
     // This requires a valid ID
     public function update_order_item($id, $params=array()) {
         $p = array();
@@ -242,7 +242,7 @@ class WhiplashApi
         }
         return $this->put('order_items/'.$id, $p);
     }
-    
+
     // This requires a valid ID
     public function delete_order_item($id) {
         return $this->delete('order_items/'.$id);
